9d683bc24cdfa0eedda0cce416e7ad64208b4877
Sébastien Guillon <sebastienguillon@gmail.com>
2019-02-01 07:49:10 +0100

    Adapt Status Bar ans Splashscreen plugins for Capacitor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AppState, DeviceInfo, Plugins, StatusBarStyle } from '@capacitor/core';
+// export { HelloWorldPlugin } from '../../node_modules/hello-world-capacitor-plugin';
 
 @Component({
   selector: 'app-root',
@@ -11,16 +10,46 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      Plugins.StatusBar.setStyle({style: StatusBarStyle.Dark});
+      Plugins.SplashScreen.hide();
+      console.log('App is ready');
+      this.listenToAppStateChange();
+      this.getDeviceInfo();
+      this.helloPluginTest();
     });
   }
+
+  private listenToAppStateChange() {
+    Plugins.App.addListener('appStateChange', (appState: AppState) => {
+      if (appState.isActive) {
+        console.log('App has become active');
+      } else {
+        console.log('App has become inactive');
+      }
+    });
+  }
+
+  private helloPluginTest(): void {
+    // const { HelloWorld } = Plugins;
+    // Plugins.HelloWorld.echo({
+    //   value: 'Hello Capacitor!'
+    // })
+    // .then(res => {
+    //   console.log('res from Hello World plugin:', res);
+    // });
+  }
+
+  private async getDeviceInfo(): Promise<void> {
+    console.log('Plugins: ', Plugins);
+    const { Device } = Plugins;
+    const deviceInfo: DeviceInfo = await Device.getInfo();
+    console.log('Device info:', deviceInfo);
+  }
+
 }
