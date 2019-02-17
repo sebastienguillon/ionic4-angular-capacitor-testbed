01066e9bd16beb8d5e975f3659a4e6aa7ca63792
Sébastien Guillon <sebastienguillon@gmail.com>
2019-02-17 15:37:42 +0100

    Add error handling service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -6,11 +6,18 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { ErrorHandlerService } from './services/error-handler.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { AppPreferences } from '@ionic-native/app-preferences/ngx';
 
+/**
+ * REMINDER:
+ * Since Angular 6, we don't need to add singleton services to app.module.ts's
+ * providers, as long as we define providedIn: 'root' in the service's
+ * @Injectable decorator.
+ */
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +32,8 @@ import { AppPreferences } from '@ionic-native/app-preferences/ngx';
     AppPreferences,
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy, }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy, },
+    { provide: ErrorHandler, useClass: ErrorHandlerService },
   ],
   bootstrap: [
     AppComponent,
