15b12792d78d0a138fafbe87a6dcb9b8e6e7acab
Sébastien Guillon <sebastienguillon@gmail.com>
2019-02-17 16:35:17 +0100

    Cleanup cli-added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },  { path: 'ionic', loadChildren: './pages/ionic/ionic.module#IonicPageModule' },
-  { path: 'sticky', loadChildren: './pages/ionic/sticky/sticky.module#StickyPageModule' },
-
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
 ];
 @NgModule({
   imports: [
