f582b325f208dc4e618cc713a24210de420b5851
Sébastien Guillon <sebastienguillon@gmail.com>
2019-05-04 20:04:54 +0200

    Remove extra route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'sticky-property', loadChildren: './pages/ionic/sticky-property/sticky-property.module#StickyPropertyPageModule' },
 ];
 @NgModule({
   imports: [
