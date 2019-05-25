8a9fbe5bf7317962a9a7b95c40e17483f70f525c
Sébastien Guillon <sebastien.guillon-ext@vinci-autoroutes.com>
2019-05-25 22:11:24 +0200

    Remove unneeded route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'geolocation-settings', loadChildren: './pages/capacitor-plugins/geolocation/modals/geolocation-settings/geolocation-settings.module#GeolocationSettingsPageModule' },
 ];
 @NgModule({
   imports: [
